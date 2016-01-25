4a55d3881dcc48437f0502516e0c9e5fd0cef4e4
zhenyuan992 <zhenyuan.yeo13@sps.nus.edu.sg>
2016-01-25 21:49:29 +0800

    first input

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("helloworld!!");
+		
+		//read the file, convert to vector list
+			// do maniuplation
+			//add display delete N clear
+				// if valid, update every operation except for display
+		
+	}
+}
